3825cbc0573d8f33efc1119d70a1dbf166de551e
Moura1 <50001134+Moura1@users.noreply.github.com>
2019-05-05 17:30:55 -0300

    Add files via upload

---
+++ perfil.php
@@ -0,0 +1,97 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Perfil</title>
+    <link rel="stylesheet" type="text/css" href="CSS/perfil.css">
+</head>
+
+<body>
+    <header class="cabecalho">
+        <nav>
+            <main>
+                <div class="header-1">
+                    <div class="logo">
+                        <img src="IMAGENS/logo.png" />
+                    </div>
+
+                    <div class="menu">
+                        <ul>
+                        <li><a href="cadastrar.php"> Cadastrar </a> </li>
+                        <li><a href="login.php"> Login </a> </li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </main>
+        </nav>
+    </header>
+
+<div class="perfil">
+    <h2>Perfil</h2>
+        <ul>
+            <li><a href="">Lista de Clientes</a></li>
+            <li><a href=""> Lista de ADM</a></li>
+            <li><a href=""> Lista de Músicas</a></li>
+            <li><a href=""> Sair</a></li>
+        </ul>
+</div>
+
+<div id="album0">
+     <figure>
+        <img src="/IMAGENS/album-pequeno.jpg" width="90" height="80" />
+        <figcaption>Figura 1 </figcaption>
+        <li>Nome: </li>
+        <li>Autor: </li>
+        <li>Album: </li>
+    <figure>
+   
+     
+</div>
+   
+<div id="album1">
+     <figure>
+       <img src="/IMAGENS/album-pequeno.jpg" width="90" height="80" />
+        <figcaption>Figura 2 </figcaption>
+    <li>Nome: </li>
+    <li>Autor: </li>
+    <li>Album: </li>
+    </figure>
+</div>
+
+<div id="album2">
+    <figure>
+        <img src="/IMAGENS/album-pequeno.jpg" width="90" height="80" />
+        <figcaption>Figura 3</figcaption>
+    <li>Nome: </li>
+    <li>Autor: </li>
+    <li>Album: </li>
+    </figure>
+</div>
+
+<div id="album3">
+    <figure>
+        <img src="/IMAGENS/album-pequeno.jpg" width="90" height="80" />
+        <figcaption>Figura 4 </figcaption>
+
+    <li>Nome: </li>
+    <li>Autor: </li>
+    <li>Album: </li>
+    </figure>
+</div>
+    
+
+
+    
+
+</body>
+
+<footer>
+<p>E:Mail</p>
+<p>Telefone</p>
+</footer>
+
+
+
+</html>
